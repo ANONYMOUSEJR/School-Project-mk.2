3b7bd04f45efa79c9cf1ffa429b435a587fa73fd
ANONYMOUSE JR <ombe2016@gmail.com>
2022-03-01 23:02:15 +0100

    Add project files.

---
+++ School Project mk.2/School Project mk.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "math.h"
+
+using namespace std;
+
+int main(){
+    math stud1;
+    stud1.questProc();
+
+    return 0;
+}
---
+++ School Project mk.2/math.h
@@ -0,0 +1,92 @@
+#pragma once
+#pragma warning( push ) // Disabling warnings Link: https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/.
+#pragma warning( disable : 4244 )
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+
+using namespace std;
+using namespace chrono;
+using namespace this_thread;
+
+
+class math {
+private:
+	unsigned short pts;
+
+public:
+	unsigned short questAmount;
+	char diff;
+	short arrEasy[4] = { 1, 2, 5, 10 }, secondVar, ans, studAns;
+
+	void questProc() {
+	start:
+		cout << "How hard do you want the questtions to be?" << endl;
+		cout << "(The harder you choose, the larger the numbers you'll have to calculate)" << endl;
+		cout << "1) EASY" << endl;
+		cout << "2) MEDIUM" << endl;
+		cout << "3) HARD" << endl;
+		cout << "4) CALCULATOR MODE LOL" << endl << "~> ";
+		cin >> diff;
+
+		switch (diff) {
+		case '1':
+			system("cls");
+			cout << "EASY MODE ACTIVATED" << endl;
+			break;
+
+		case '2':
+			system("cls");
+			cout << "MEDIUM MODE ACTIVATED" << endl;
+			break;
+
+		case '3':
+			system("cls");
+			cout << "HARD MODE ACTIVATED" << endl;
+			break;
+
+		case '4':
+			system("cls");
+			cout << "CALCULATOR MODE ACTIVATED" << endl;
+			break;
+
+		default:
+			system("cls");
+			cout << "INPUTED VAL IS EITHER NOT AN INT OR ISN'T IN THE LIST"; // Checks only the starting int ignores the rest...
+			sleep_for(0.5s); cout << "."; sleep_for(0.5s); cout << "."; sleep_for(0.5s); cout << "."; sleep_for(0.5s);
+			cin.get();
+			break;
+
+			if (diff != 1 && diff != 2 && diff != 3 && diff != 4) { goto start; } // Try making this more optimised, get rid of the if statement and get on with your life...
+		}
+
+		// The following creates the question according to the set difficulty.
+		srand(time(NULL)); // Initializing random seed.
+		switch (diff) {
+		case '1': // Easy Mode:
+			system("cls");
+			random_shuffle(&arrEasy[0], &arrEasy[4]); // For first var.
+			secondVar = rand() % 10 + 1; // Rand number from 1 to 10 (for second var).
+			ans = arrEasy[0] * secondVar;
+			cout << arrEasy[0] << " * " << secondVar << " = ?" << endl << "~> ";
+			cin >> studAns;
+			break;
+
+		default:
+			break;
+		}
+
+		// The following checks if ans is correct.
+
+		if (studAns == ans) {
+			cout << "WELL DONE!";
+		}
+		else {
+			cout << "WRONG, CORRECT ANSWER WAS: " << ans;
+		}
+	}
+};
+
+#pragma warning ( pop )
